e3d642fdd80ae911b8b23ef0a1e9e11c2427060f
nicolas-garcess <nicolasgarces95@gmail.com>
2021-04-17 11:13:34 -0500

    Feat: Agregada la funcionalidad de buscar el promedio del precio por metro cuadrado. Además, se modificó la estructura de la funcionalidad de filtrar datos por precio y/o habitaciones

--- src/controllers/apartmentscontroller.js
+++ src/controllers/apartmentscontroller.js
@@ -2,38 +2,49 @@ const ApartmentModel = require('../models/apartments.model');
 
 const Controller = {};
 
+const defineCriteria = (headers) => {
+    let response = {};
+        if (isPriceAndRooms(headers)) {
+            response = {$and: [{Precio: { $lte: headers.maxprice || 1000000, $gte: headers.minprice || 0}}, {Habitaciones: headers.rooms}]};
+        } else if (isOnlyPrice(headers)) {
+            response = {Precio: { $lte: headers.maxprice || 1000000, $gte: headers.minprice || 0}};
+        } else if (isOnlyRooms(headers)) {
+            response = {Habitaciones: headers.rooms};
+        } 
+    
+    return response;
+}
+
+const isPriceAndRooms = (headers) => {
+    return ((headers.maxprice || headers.minprice) 
+            && headers.rooms) 
+            ? true : false;
+}
+
+const isOnlyPrice = (headers) => {
+    return (headers.maxprice || headers.minprice) 
+            ? true : false;
+}
+
+const isOnlyRooms = (headers) => {
+    return headers.rooms ? true : false;
+}
+
 Controller.search = async (req, res) => {
     try {
-        const requestResult = req.headers;
-        let criteria = {};
-        if ((requestResult.maxprice
-            || requestResult.minprice)
-            && requestResult.rooms) {
-
-                criteria = {$and: [{Precio: { $lte: requestResult.maxprice || 1000000, $gte: requestResult.minprice || 0}}, {Habitaciones: requestResult.rooms}]};
-
-        } else if (requestResult.maxprice
-                  || requestResult.minprice) {
-
-                    criteria = {Precio: { $lte: requestResult.maxprice || 1000000, $gte: requestResult.minprice || 0}};
-
-        } else if (requestResult.rooms) {
-            
-            criteria = {Habitaciones: requestResult.rooms};
-
+        const criteria = defineCriteria(req.headers);        
+        
+        if (Object.keys(criteria).length !== 0) {
+            ApartmentModel.find(criteria)
+            .then((r) => res.status(200).json({
+                message: "Búsqueda exitosa",
+                quantity: r.length
+            }))
+            .catch(e => res.status(404).json(e))
         } else {
             res.status(400).json({
                 message: "No ingresó ningún campo"
             });
-        }
-        
-        if (Object.keys(criteria).length !== 0) {
-            await ApartmentModel.find(criteria)
-            .then(r => console.log(r.length))
-            .then(() => res.status(200).json({
-                message: "Búsqueda exitosa"
-            }))
-            .catch(e => res.status(404).json(e))
         }
     } catch (error) {
         console.log(error);
@@ -42,14 +53,48 @@ Controller.search = async (req, res) => {
 
 Controller.process = async (req, res) => {
     try {
-        const latitude = req.headers.latitude;
-        const longitude = req.headers.longitude;
-        const distance = req.distance 
+        //Se deben convertir a radianes porque el paquete Math opera en radianes.
+        const latitude = Number(req.headers.latitude) * Math.PI/180;
+        const longitude = Number(req.headers.longitude) * Math.PI/180;
+        const distance = Number(req.headers.distance); 
 
+        const data = await ApartmentModel.find();
+
+        let arrivalLatitude = 0;
+        let arrivalLongitude = 0;
+        let longitudeDifference = 0;
+        let distanceTwoPoints = 0;
+        let countApartments = 0;
+        let plusPricePerSquareMeter = 0;
+
+        for (const row of data) {
+            arrivalLatitude = row.Latitud * Math.PI/180;
+            arrivalLongitude = row.Longitud * Math.PI/180;
+            longitudeDifference = arrivalLongitude - longitude;
+
+            //Distancia en km, ecuación para el cálculo de la ortodrómica entre dos puntos
+            distanceTwoPoints = Math.acos(Math.sin(latitude)*Math.sin(arrivalLatitude) + Math.cos(latitude)*Math.cos(arrivalLatitude)*Math.cos(longitudeDifference))*(180/Math.PI)*111.1;
+
+            if (distanceTwoPoints <= distance) {
+                countApartments = countApartments + 1;
+                plusPricePerSquareMeter = plusPricePerSquareMeter + row.Precio_por_metro;
+            }
+        }
+
+        let meanPricePerSquareMeter = plusPricePerSquareMeter/countApartments;
+
+        res.status(200).json({
+            message: "Consulta con éxito",
+            value: meanPricePerSquareMeter.toFixed(2)
+        });
 
     } catch (error) {
         console.log(error);
     }
+}
+
+Controller.exportsFile = (req, res) => {
+
 }
 
 module.exports = Controller;
